3af745636bb4d02cec8e9048d299f57a281447e3
Tatyana <breatninsideme@gmail.com>
2023-10-10 20:31:46 +0400

    Create model City

--- WeatherApp/weather/models.py
+++ WeatherApp/weather/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    city_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.city_name
